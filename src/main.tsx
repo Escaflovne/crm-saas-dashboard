09ce31877589f1a1f4cfe66327c414b932a2c9d4
TheCitizenOfTheFuture <ad.astra@tutamail.com>
2026-05-13 01:28:40 +0700

    feat: add font imports and reset layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
